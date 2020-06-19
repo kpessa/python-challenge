7f451e559b72bc2d030a1ddbbf783be674e709f2
Kurt Pessa <kpessa@gmail.com>
2020-06-19 12:05:27 -0400

    finished PyBank challenge, pushing to github

---
+++ PyBank/main.py
@@ -0,0 +1,45 @@
+import os
+import csv
+
+csvpath = os.path.join('Resources', 'budget_data.csv')
+
+with open(csvpath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+
+    #Read each row of data after the header
+    netTotal = 0
+    numMonths = 0
+    greatestIncrease = ["",0]
+    greatestDecrease = ["",0]
+    
+    for row in csvreader:
+        currentProfitLoss = int(row[1])
+        if currentProfitLoss > int(greatestIncrease[1]):
+            greatestIncrease = row
+        if currentProfitLoss < int(greatestDecrease[1]):
+            greatestDecrease = row 
+
+        netTotal += currentProfitLoss
+        numMonths += 1
+
+    printStr = f"\nTotal Months: {numMonths}\n"    
+    printStr += f"Total: ${netTotal:,}\n"
+    printStr += f"Average Change: ${round(netTotal/numMonths,2):,}\n"
+    printStr += f"Greatest Increase in Profits: {greatestIncrease[0]} (${int(greatestIncrease[1]):,})\n" 
+    printStr += f"Greatest Decrease in Profits: {greatestDecrease[0]} (${int(greatestDecrease[1]):,})\n"
+
+    print(printStr)
+
+    outputfilepath = os.path.join("analysis",'analysis.txt')
+    with open(outputfilepath,'w') as f:
+        f.write(printStr)
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# python-challenge
+# Python Challenge
+###### by Kurt Pessa
+
+#### Setup
+* Created a repository `python-challenge`
+* Inside repository, created directories `PyBank` and `PyPoll` for both challenges
+
+## PyBank
+
+#### Setup
+* Created a new python file `main.py` that is the main script to run for the analysis
+1. Added a `Resources` folder that contains a CSV file `budget_data.csv`
+2. Added an `analysis` folder that contains a text file with the results from the analysis
+
+ 
